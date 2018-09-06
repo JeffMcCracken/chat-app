c8e3ff3b0d944660cde651c65010f0a359b201ad
Jeffrey McCracken <jmccracken@c21scheetz.com>
2018-09-06 09:36:29 -0400

    Add geolocation support via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -12,13 +12,25 @@ socket.on('disconnect', function () {
 
 //New message listener
 socket.on('newMessage', function (message) {
-    console.log('New message', message);
     var li = jQuery('<li></li>');
     li.text(`${message.from}: ${message.text}`);
 
     jQuery('#messages').append(li);
 });
 
+//New location message listener
+socket.on('newLocationMessage', function (message) {
+    var li = jQuery('<li></li>');
+    var a = jQuery('<a target="_blank">My current location</a>');
+
+    li.text(`${message.from}: `);
+    a.attr('href', message.url);
+    li.append(a);
+
+    jQuery('#messages').append(li);
+});
+
+//Custom even listener for sending message
 jQuery('#message-form').on('submit', function(e) {
     e.preventDefault();
 
@@ -28,4 +40,20 @@ jQuery('#message-form').on('submit', function(e) {
     }, function () {
 
     });
+});
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function (e) {
+    if (!navigator.geolocation) {
+        return alert('Geolocation not supported by your browser');
+    }
+
+    navigator.geolocation.getCurrentPosition(function (position) {
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    }, function () {
+        alert('Unable to fetch location');
+    })
 });
